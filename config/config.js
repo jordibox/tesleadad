541e59394c1652a666657b3c442625ec6a4a8ed5
Xavier Serrano <zombispormedio007@gmail.com>
2015-10-13 09:29:25 +0200

    oauth

--- config/config.js
+++ config/config.js
@@ -2,7 +2,9 @@ var config = (function () {
     var private = {
 
         port: 5000,
-        
+
+        db:"mongodb://pickuser:claudia5@ds051990.mongolab.com:51990/pickyourdaydb"
+
     };
     return {
         get: function (name) {
--- server.js
+++ server.js
@@ -8,22 +8,33 @@ require("./config/express.js")(app);
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function(req, res) {
-	res.jsonp({message:"Welcome to PickYourDay!!"});
-})
+app.get('/', function (req, res) {
+    res.jsonp({ message: "Welcome to PickYourDay!!" });
+});
 
 
 var router=express.Router();
 
 router.route("")
 .get(function(req, res){
-	res.jsonp({message:"Welcome to PickYourDay API!!"});
+    res.jsonp({message:"Welcome to PickYourDay API!!"});
 });
+
+var auth_router=express.Router();
+auth_router.route("")
+    .get(function(req, res){
+    res.jsonp({message:"Do you want register?"});
+});
+
+router.use("/oauth", auth_router);
+
 
 app.use("/api", router);
 
+
+
 app.listen(app.get("port"), function(){
-    console.log("Magia!!!");
+    console.log("Conectado: "+app.get("port"));
 });
 
 
