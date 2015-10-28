de6b3669e58fe1419de9c37f0999bdba76d750c4
Xavier Serrano <zombispormedio007@gmail.com>
2015-10-28 18:35:03 +0100

    not localhost

--- config/database.js
+++ config/database.js
@@ -10,7 +10,7 @@ module.exports=function(){
     mongodb.on("error", function(err){
         console.log("Ha ocurrido este error durante la conexion con mongodb: "+err.message);
 		console.log("Try to Localhost");
-		mongodb=mongoose.connect(C.db_secure).connection;
+		
 		});
 
     mongodb.once('open', function(){
