6f56e66934c1b7ac4209ebcebbb17e7242b78fe9
Xavier Serrano <zombispormedio007@gmail.com>
2015-11-29 18:14:57 +0100

    control accesss ocrs

--- config/express.js
+++ config/express.js
@@ -1,10 +1,10 @@
 var express = require("express");
-var morgan =require("morgan");
-var bodyParser=require("body-parser");
-var methodOverride=require("method-override");
+var morgan = require("morgan");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
 
 
-module.exports=function(app){
+module.exports = function (app) {
 
 
     //Configuracion
@@ -14,13 +14,17 @@ module.exports=function(app){
     app.use(morgan('dev'));
 
     //Permite cambiar el html con el método POST
-    app.use(bodyParser.urlencoded({extended:false})); //parse x-www-form-urlencoded
+    app.use(bodyParser.urlencoded({ extended: false })); //parse x-www-form-urlencoded
 
     app.use(bodyParser.json()); //parsea json;
 
     //Simula delete y put
     app.use(methodOverride());
-
+    app.use(function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
 
 
 
--- public/app.js
+++ public/app.js
@@ -23,6 +23,9 @@ var app = angular.module('myAdmin', ["ngRoute"])
 
 
 		$httpProvider.defaults.useXDomain = true;
-        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+		$httpProvider.defaults.withCredentials = true;
+		delete $httpProvider.defaults.headers.common["X-Requested-With"];
+		$httpProvider.defaults.headers.common["Accept"] = "application/json";
+		$httpProvider.defaults.headers.common["Content-Type"] = "application/json";
 
 	});
