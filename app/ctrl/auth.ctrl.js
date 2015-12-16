2dcae78dcbef4e02519654c854189bc3f64f743c
Jorge <jordinformatica@gmail.com>
2015-12-16 20:31:28 +0100

    error

--- app/ctrl/auth.ctrl.js
+++ app/ctrl/auth.ctrl.js
@@ -77,7 +77,7 @@ AuthController.logout=function(token, cb){
 }
 
 AuthController.check=function(query, cb){
-	if(!query.email) return cb("Fields not filled"):
+	if(!query.email) return cb("Fields not filled");
 	AuthModel.findOne({email: query.email},function(err, user){
 		if(err)return cb(err);
 
